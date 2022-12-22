bb4d53d6660709b2e5d0a9ef7a13bf12bb4e5988
Christian Kozalla <christian.kozalla@gmail.com>
2022-12-22 09:41:35 +0100

    fix: separate faq.js, remove function.js where no toc is present

---
+++ scripts/faq.js
@@ -0,0 +1,9 @@
+// Code for FAQ
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
