c01857118160f2d2fb56cfb6513a525fd9f74073
Magpie <anna.maria.weinhold@posteo.de>
2023-01-22 19:27:14 +0300

    stand 22.01.

---
+++ scripts/main-navbar-en.js
@@ -0,0 +1,375 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+* {
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+}
+    .logo {
+        padding-left: 50px;
+        padding-top: 10px;
+    }
+
+    .navbar {
+        height: var(--navbar-height);
+        width: 100%;
+        background: var(--background-color);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 3;
+        box-shadow: 1px 1px 4px #d2d2d2;    
+    }
+
+    .navbar__ul {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        width: 100%;
+        align-items: center;
+        list-style: none;
+        position: relative;
+    }
+
+    .navbar__lang {
+        margin-right: 20px;
+    }
+
+    .navbar__li {
+        position: relative;
+        font-size: 22px;
+        font-size: 1.375rem;
+        font-family: 'Quattrocento', Arial, Helvetica, sans-serif;
+        padding: 20px;
+        margin-right: 40px;
+    }
+
+    .link:hover {
+        color: #ff6b35;   
+    }
+
+    .link {
+        text-decoration: none;
+        color: var(--text-color);
+    }
+
+    .toggle-button {
+        width: 35px;
+        height: 25px;
+        position: absolute;
+        top: 25px;
+        right: 25px;
+        display: none;
+        flex-direction: column;
+        justify-content: space-between;
+        cursor: pointer;
+    }
+
+    .bar {
+        height: 4px;
+        width: 100%;
+        background: #171614;
+        border-radius: 4px;
+    }
+
+    .navbar input[type="checkbox"] {
+        display: none;
+    }
+
+    .nav--active {
+        color: #ff6b35;
+    }
+
+    .nav__lang {
+        color: #fff;
+        background-color: #ff6b35;
+        font-size: 16px;
+        font-size: 1rem;
+        text-decoration: none;
+    }
+
+    .nav__lang:hover {
+    cursor: pointer;
+    color: #fff;
+    }
+
+    .sub-navbar__icon {
+        cursor: pointer;
+    }
+
+    .sub-navbar {
+      list-style: none;
+      position: absolute;
+      top: 70px;
+      flex-direction: column;
+      justify-content: center;
+      align-items: start;
+      background-color: #fff;
+      opacity: 0.98;
+      display: none;
+      margin-left: 60px;
+    }
+
+    .sub-navbar__dropdown {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+    
+    .sub-navbar__dropdown:hover {
+        color: #ff6b35;
+    }
+
+    nav li:hover .sub-navbar {
+        display: flex;
+    }
+
+    .sub-navbar li a {
+        justify-content: flex-start;
+        font-size: 1.25rem;
+    }
+
+    .navbar__button {
+        margin-right: 40px;
+    }
+
+    .navbar__button a {
+        text-decoration: none;
+        color: #fff;
+        background: #ff6b35;
+        padding: 10px 15px;
+        font-size: 18px;
+        font-size: 1.25rem;
+        font-weight: bold;
+        border: 1px solid #ff6b35;
+    }
+
+    .navbar__button a:hover {
+        color: #ff6b35;
+        background: #fff;
+    }
+
+    .toggle-button span.bar {
+        transition: transform 200ms linear, opacity 100ms linear;
+    }
+    .toggle-button span.bar:nth-child(1) {
+        transform: translateY(0) rotate(0deg);
+    }
+    .toggle-button span.bar:nth-child(2) {
+        opacity: 1;
+    }
+    .toggle-button span.bar:nth-child(3) {
+        transform: translateY() rotate(0deg);
+    }
+    #toggle-button:checked ~ .toggle-button span.bar:nth-child(1) {
+        transform: translateY(10.5px) rotate(135deg);
+    }
+    #toggle-button:checked ~ .toggle-button span.bar:nth-child(2) {
+        opacity: 0;
+    }
+    #toggle-button:checked ~ .toggle-button span.bar:nth-child(3) {
+        transform: translateY(-10.5px) rotate(-135deg);
+    }
+
+    @media only screen and (min-width:760px) and (max-width:979px){
+        header {
+            padding: 0;
+        }
+       
+       .logo {
+        padding: 0;
+        width: 100px;
+        margin-left: 20px;
+        margin-top: 10px;
+       }
+    
+       .logo:hover {
+        border-bottom: none;
+        cursor: pointer;
+       }
+    
+        .toggle-button {
+            display: flex;
+        }
+    
+        .navbar {
+            height: 80px;
+        }
+    
+        .navbar__ul {
+            height: min-content;
+            width: 100%;
+            background: #fff;
+            display: none;
+            position: absolute;
+            top: 80px;
+            left: 0;
+            padding-left: 40px;
+            opacity: 0.98;
+        }
+
+        .navbar__lang {
+            margin-bottom: 30px;
+        }
+
+        .sub-navbar__dropdown {
+            display: block;
+            margin-top: 40px;
+        }
+    
+        .sub-navbar__dropdown:hover .sub-navbar {
+            display: none;
+        }
+        
+        .sub-navbar__dropdown input[type="checkbox"]:checked ~ .sub-navbar {
+            display: block;
+        }
+        
+        .sub-navbar {
+            position: static;
+            width: 100%;
+            margin-left: 0;
+        }
+  
+        .navbar__li {
+            height: min-content;
+            width: 100%;
+            margin-left: 0;
+        }
+
+        .navbar__button {
+            margin-left: 20px;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+     
+     
+    
+        #toggle-button:checked ~ ul {
+            display: block;
+        }
+    }    
+    
+    @media only screen and (max-width:759px){
+        header {
+            padding: 0;
+        }
+       
+       .logo {
+        padding: 0;
+        width: 100px;
+        margin-left: 20px;
+        margin-top: 10px;
+       }
+    
+       .logo:hover {
+        border-bottom: none;
+        cursor: pointer;
+       }
+    
+        .toggle-button {
+            display: flex;
+        }
+    
+        .navbar {
+            height: 80px;
+        }
+    
+        .navbar__ul {
+            height: min-content;
+            width: 100%;
+            background: #fff;
+            display: none;
+            position: absolute;
+            top: 80px;
+            left: 0;
+            padding-right: 30px;
+            text-align: right;
+            opacity: 0.98;
+        }
+    
+        .navbar__li {
+            height: min-content;
+            width: 100%;
+            margin-right: 0;
+        }
+
+        .navbar__lang {
+            margin-bottom: 30px;
+        }
+
+        .sub-navbar__dropdown {
+            display: block;
+            margin-top: 40px;
+        }
+    
+        .sub-navbar__dropdown:hover .sub-navbar {
+            display: none;
+        }
+        
+        .sub-navbar__dropdown input[type="checkbox"]:checked ~ .sub-navbar {
+            display: block;
+        }
+        
+        .sub-navbar {
+            position: static;
+            width: 100%;
+            margin-left: 0;
+        }
+
+        .navbar__button {
+            margin-right: 20px;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+  
+    
+        #toggle-button:checked ~ ul {
+            display: block;
+        }
+    }
+</style>
+
+<header>
+<nav class="navbar">
+    <a href="/" class="logo"><img src="/images/logo.svg" width="150"
+            alt="Logo Inloopo"></a>
+    <input type="checkbox" id="toggle-button">
+    <label for="toggle-button" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </label>
+    <ul class="navbar__ul">
+       <li class="sub-navbar__dropdown">
+        <input type="checkbox" id="sub-navbar__checkbox">
+        <label for="sub-navbar__checkbox" class="navbar__li">Products</label>
+            <ul class="sub-navbar">
+                <li class="navbar__li"><a class="link" href="investment">Signal Service</li>
+                <li class="navbar__li"><a class="link" href="wikifolio">Managed Account</li>
+            </ul>
+        </li>
+        <li class="navbar__li"><a class="link" href="about">About</a></li>
+        <li class="navbar__li"><a class="link" href="contact">Contact</a></li>
+        <li class="navbar__button"><a href="e-book-coming-soon">Free E-Book</a></li>
+        <li class="navbar__li  navbar__lang"><a class="link" href="/de/">DE</a></li>
+    </ul>
+</nav>
+</header>
+`
+
+class MainNavBar extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('main-navbar-en', MainNavBar);
---
+++ scripts/site-footer.js
@@ -0,0 +1,128 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+* {
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+}
+
+.site-footer{
+    background: var(--dark-background-color);
+    padding: 90px 0;
+    width: 100%;
+    float: left;
+    margin-top: 90px;
+}
+
+.site-footer__link--center{
+    color: #fff;
+    text-decoration: none;
+    padding: 0 10px;
+}
+
+.site-footer__link--center:hover{
+    color: var(--accent-color);
+}
+
+.site-footer__text--center{
+    color: #fff;
+    margin-bottom: 25px;  
+    text-align: center; 
+}
+
+.site-footer__text{
+    color: #fff;
+    margin-bottom: 25px;
+    font-size: 14px;
+    font-size: 0.875rem;
+    line-height: 1.7; 
+    padding-left: 25px;
+    padding-right: 25px;
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.site-footer__text--legal {
+    padding-top: 60px;
+}
+
+.site-footer__heading{
+    color: #fff;
+    text-align: center;
+    margin-bottom: 5px;
+}
+
+@media only screen and (max-width:759px){ 
+
+    .site-footer__text {
+        width: 90%;
+    }
+
+}
+
+@media only screen and (min-width:760px) and (max-width:979px) {
+   
+    .site-footer__text {
+        width: 90%;
+    }
+
+}
+
+</style>
+
+<footer class="site-footer">
+        <section>
+            <h2 class="site-footer__heading">inloopo</h2>
+            <p class="site-footer__text--center">Dein Investment. Deine Freiheit.</p>
+            <p class="site-footer__text--center">
+                <a class="site-footer__link--center" href="datenschutz">Datenschutz</a> |
+                <a class="site-footer__link--center" href="nutzungsbedingungen">Nutzungsbedingungen</a> |
+                <a class="site-footer__link--center" href="risikohinweis">Risikohinweis</a> |
+                <a class="site-footer__link--center" href="impressum">Impressum</a>
+            </p>
+            <p class="site-footer__text--center">Copyright © 2022 KW³ Business Ltd</p>
+
+            <div class="wrapper-content  site-footer__text--legal">
+                <p class="site-footer__text">Risiko Offenlegung: Der Handel mit ETFs, Futures oder Aktien birgt ein
+                    hohes
+                    Risiko und ist nicht für jeden Investor geeignet. Ein Investor kann möglicherweise mehr als das
+                    eingezahlte Kapital verlieren. Für den Handel sollte nur Risikokapital verwendet werden, bzw. Teile
+                    des
+                    Risikokapitals. Risikokapital ist Geld, bei dessen Verlust sich keine Änderung der Finanzsituation
+                    ergibt bzw. keinen Einfluss auf das Leben mit sich bringt. Eine in der Vergangenheit erzielte
+                    Performance ist keine Garantie für zukünftige Gewinne.</p>
+                <p class="site-footer__text">
+                    Offenlegung der hypothetischen Performance: Hypothetische Performanceergebnisse haben viele
+                    inhärente
+                    Einschränkungen, von denen einige im Folgenden beschrieben werden. Die dargestellten Ergebnisse des
+                    Kontos können in den Gewinnen und Verlusten stark abweichen. Einer der Einschränkungen der
+                    hypothetischen Ergebnisse ist, dass Sie durch bekannte historische Daten entstanden sind. Darüber
+                    hinaus
+                    beinhaltet der hypothetische Handel kein finanzielles Risiko – kein hypothetischer Track Record kann
+                    die
+                    finanziellen Risiken des tatsächlichen Handels darstellen. Beispielsweise besteht die Möglichkeit,
+                    dass
+                    der Handel bei Verlusten ausgesetzt bzw. abgebrochen wird, dies kann die tatsächlichen Ergebnisse
+                    stark
+                    verändern. Des Weiteren gibt es zahlreiche weitere Faktoren die bei der Umsetzung eines
+                    Handelsprogramms
+                    nicht vollständig in der hypothetischen Performance berücksichtigt werden können und somit die
+                    tatsächlichen Ergebnisse beeinflussen können.</p>
+            </div>
+        </section>
+        </div>
+    </footer>
+`
+
+class SiteFooter extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('site-footer', SiteFooter);
